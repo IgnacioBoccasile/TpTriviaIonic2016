cc977f4a7ab842759a0ff3c815ddb3e94f974430
IgnacioBoccasile <ignacioboccasile@hotmail.com>
2016-09-21 14:15:33 -0300

    Proyecto iniciado con tabs modificados.

---
+++ www/js/app.js
@@ -0,0 +1,57 @@
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+
+  .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  .state('tab.autor', {
+    url: '/autor',
+    views: {
+      'tab-autor': {
+        templateUrl: 'templates/tab-autor.html',
+        controller: 'AutorCtrl'
+      }
+    }
+  })
+
+  .state('tab.trivia', {
+      url: '/trivia',
+      views: {
+        'tab-trivia': {
+          templateUrl: 'templates/tab-trivia.html',
+          controller: 'TriviaCtrl'
+        }
+      }
+    })
+
+  .state('tab.inicio', {
+    url: '/inicio',
+    views: {
+      'tab-inicio': {
+        templateUrl: 'templates/tab-inicio.html',
+        controller: 'InicioCtrl'
+      }
+    }
+  });
+
+  $urlRouterProvider.otherwise('/tab/inicio');
+
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,7 @@
+angular.module('starter.controllers', [])
+
+.controller('AutorCtrl', function($scope) {})
+
+.controller('TriviaCtrl', function($scope) {})
+
+.controller('InicioCtrl', function($scope) {});
